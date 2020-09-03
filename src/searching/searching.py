7124e982e799d92385640d9ae753f3ded49d56ef
Cody Brewer <codybrewerdev@gmail.com>
2020-09-02 18:35:43 -0600

    linear search completed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,15 @@
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
+    # loop through each item in the array
+    for item in arr:
+        # check if the item in the array is equal to the target
+        if item == target:
+            # if the item is equal to the target return the index of the item
+            return arr.index(item)
+    return -1 # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
     # Your code here
-
-
     return -1  # not found
