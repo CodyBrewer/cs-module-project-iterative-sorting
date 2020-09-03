944249a05ba9497d728bd24840d59f851655b3ce
Cody Brewer <codybrewerdev@gmail.com>
2020-09-02 18:44:59 -0600

    binary search method completed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -10,6 +10,26 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    # check if the length of the array has at least one item
+    if 0 < len(arr):
+        # set the starting pointer value to 0
+        start_pointer = 0
+        # set the end_pointer to the length of the array minus one to get the last index value
+        end_pointer = len(arr) - 1
+        # while the starting pointer value is less than the end pointer value
+        while start_pointer <= end_pointer:
+            # find the midpoint index betweent he start and end pointer
+            midpoint_pointer = (start_pointer + end_pointer) // 2
+            # check if the target is less than the index of the value of the midpoint index 
+            if target < arr[midpoint_pointer]:
+                # reset the end_pointer to the midpoint
+                end_pointer = midpoint_pointer
+            # else check if the target is greater than the index of the value of the midpoint index
+            elif target > arr[midpoint_pointer]:
+                # if it is set the starting point to the midpoint
+                start_pointer = midpoint_pointer
+            else:
+                # else the target is equal to the midpoint
+                return midpoint_pointer
     return -1  # not found
